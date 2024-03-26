e4b6d415b87dfe25c8ef5b638e23d2ac665337d2
doc <docdaystar@gmail.com>
2024-03-26 23:40:29 +0300

    change

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 31, num2 = 2;
+
+let multiply = num1 * num2;
+
+let num3=10, num4=6;
+
+let random = Math.floor(Math.random()*10)
+
+let mod = num3%num4;
+
+let max = Math.max(2, 5, 6, 20, 9);
+
+console.log(mod)
